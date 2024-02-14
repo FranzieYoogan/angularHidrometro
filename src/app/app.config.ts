6f0ad06ae4ec1e29191318306985cfbe86a3d79e
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-02-13 23:38:28 -0300

    someChatBotDesign

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+import { CalculusPageComponent } from './calculus-page/calculus-page.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),HeaderComponent,CalculusPageComponent]
 };
--- src/app/calculus-page/calculus-page.component.ts
+++ src/app/calculus-page/calculus-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-calculus-page',
   standalone: true,
-  imports: [],
+  imports: [CalculusPageComponent,HeaderComponent],
   templateUrl: './calculus-page.component.html',
   styleUrl: './calculus-page.component.css'
 })
