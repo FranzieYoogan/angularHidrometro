14c11e82a1105dc567c861eef87beecf41f6b4fb
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-02-17 17:59:55 -0300

    panelLogic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header.component';
 import { CalculusPageComponent } from './calculus-page/calculus-page.component';
+import { PanelComponent } from './panel/panel.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(),HeaderComponent,CalculusPageComponent]
+  providers: [provideRouter(routes), provideClientHydration(),HeaderComponent,CalculusPageComponent,PanelComponent]
 };
--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-panel',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './panel.component.html',
   styleUrl: './panel.component.css'
 })
-export class PanelComponent {
+export class PanelComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    this.getData()
+    this.getLitre()
+  
+  }
+
+
+  calculus() {
+    const inputValueDias:any = document.getElementById('inputValueDias')
+    const inputValueMoradores:any = document.getElementById('inputValueMoradores')
+    const inputValueAliquota:any = document.getElementById('inputValueAliquota')
+    const resultInput:any = document.getElementById('resultInput')
+    const previousOne:any = localStorage.getItem("previousOne")
+    const currentOne:any = localStorage.getItem('currentOne')
+
+    const untilNow = previousOne - currentOne
+
+    let result =  ((untilNow*1000/inputValueDias.value )/inputValueMoradores.value) + inputValueAliquota.value*100
+    console.log(untilNow)
+   
+
+    if(result) {
+      inputValueDias.value = ""
+      inputValueMoradores.value = ""
+      inputValueAliquota.value = ""
+      return   resultInput.innerHTML =  result.toFixed(2).toString()
+ 
+    }
+  
+    return   resultInput.innerHTML =  result.toString()
+
+  }
+
+ 
+
+
+  getData() {
+
+     const previousOne:any = localStorage.getItem("previousOne")
+     const currentOne:any = localStorage.getItem('currentOne')
+   
+
+
+     if(localStorage.getItem("previousOne") && localStorage.getItem("currentONe")) {
+      let untilNow:any = 0
+
+    return  untilNow
+
+     } else {
+      let untilNow:any = 0
+     
+
+      return untilNow 
+
+     }
+
+
+
+
+  }
+
+  getLitre() {
+    const previousOne:any = localStorage.getItem("previousOne")
+    const currentOne:any = localStorage.getItem('currentOne')
+
+    const untilNow = previousOne - currentOne
+
+    const litrerValue:any = untilNow * 1000
+
+    return litrerValue
+
+  }
+
 
 }
